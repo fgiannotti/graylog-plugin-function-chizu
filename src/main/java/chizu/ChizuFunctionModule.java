d81854965860fd7c58aa410ea58a10135d2b0fdf
fgiannotti <franco.giannotti@mercadolibre.com>
2021-02-09 14:01:42 -0300

    change invocation name

--- src/main/java/chizu/ChizuFunctionModule.java
+++ src/main/java/chizu/ChizuFunctionModule.java
@@ -16,6 +16,12 @@
  */
 package chizu;
 
+import com.google.inject.Binder;
+import com.google.inject.TypeLiteral;
+import com.google.inject.multibindings.MapBinder;
+import org.graylog.plugins.pipelineprocessor.ast.functions.Function;
+import org.graylog2.plugin.PluginModule;
+
 import org.graylog2.plugin.PluginConfigBean;
 import org.graylog2.plugin.PluginModule;
 
@@ -34,6 +40,19 @@ public class ChizuFunctionModule extends PluginModule {
     @Override
     public Set<? extends PluginConfigBean> getConfigBeans() {
         return Collections.emptySet();
+    }
+
+    protected void addMessageProcessorFunction(String name, Class<? extends Function<?>> functionClass) {
+        addMessageProcessorFunction(binder(), name, functionClass);
+    }
+
+    public static MapBinder<String, Function<?>> processorFunctionBinder(Binder binder) {
+        return MapBinder.newMapBinder(binder, TypeLiteral.get(String.class), new TypeLiteral<Function<?>>() {});
+    }
+
+    public static void addMessageProcessorFunction(Binder binder, String name, Class<? extends Function<?>> functionClass) {
+        processorFunctionBinder(binder).addBinding(name).to(functionClass);
+
     }
 
     @Override
@@ -56,5 +75,7 @@ public class ChizuFunctionModule extends PluginModule {
          *
          * addConfigBeans();
          */
+        addMessageProcessorFunction(ChizuFunction.NAME, ChizuFunction.class);
+
     }
 }
